7ac70112f20e8db6598b44a95e913d82473e9f26
vagrant <vagrant@homestead>
2018-04-10 12:13:01 +0000

    basic user profile editing

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,14 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Model\User;
+use App\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功！');
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
     //
     public function show(User $user)
     {
-        $test = ['name'=>'冉'];
+        $this->authorize('update',$user);
+        $test = ['name'=>'测试'];
         return view('users.show',compact('user','test'));
     }
 }
