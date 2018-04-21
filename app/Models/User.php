b1e2a9f3ea8d1d04300ff91563a1cbca3aac7134
vagrant <vagrant@homestead>
2018-04-21 07:25:04 +0000

    generated topics

---
+++ app/Models/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $table = 'users';
+
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *s
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'introduction', 'avatar'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
