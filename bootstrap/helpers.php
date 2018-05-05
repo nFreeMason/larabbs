daba444ff5bef89a02c62afa38fb77c623902239
vagrant <vagrant@homestead>
2018-05-05 09:12:54 +0000

    user can create topic

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,5 +1,10 @@
 <?php
 
+function make_excerpt($value,$length = 200)
+{
+	$excerpt = trim(preg_replace('/\r\n|\r|rn/',' ',strip_tags($value)));
+	return str_limit($excerpt, $length);
+}
 
 function route_class()
 {
